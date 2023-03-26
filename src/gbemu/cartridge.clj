3f33c0afb802b34f466a356f70b3891755072728
Travis Jefferson <traviscjefferson@gmail.com>
2023-03-26 17:49:51 -0400

    fix binary cartridge reading
    fixure out some hex/ascii mapping

--- src/gbemu/cartridge.clj
+++ src/gbemu/cartridge.clj
@@ -1,234 +1,110 @@
-(ns gbemu.cartridge)
+(ns gbemu.cartridge
+  (:require [clojure.java.io :as io]))
 
-(def ctx (atom {}))
+(def rom-types
+ {0x00 "ROM ONLY",
+  0x01 "MBC1",
+  0x02 "MBC1+RAM",
+  0x03 "MBC1+RAM+BATTERY",
+  0x05 "MBC2",
+  0x06 "MBC2+BATTERY",
+  0x08 "ROM+RAM 1",
+  0x09 "ROM+RAM+BATTERY 1",
+  0x0B "MMM01",
+  0x0C "MMM01+RAM",
+  0x0D "MMM01+RAM+BATTERY",
+  0x0F "MBC3+TIMER+BATTERY",
+  0x10 "MBC3+TIMER+RAM+BATTERY",
+  0x11 "MBC3",
+  0x12 "MBC3+RAM 2",
+  0x13 "MBC3+RAM+BATTERY 2",
+  0x19 "MBC5",
+  0x1A "MBC5+RAM",
+  0x1B "MBC5+RAM+BATTERY",
+  0x1C "MBC5+RUMBLE",
+  0x1D "MBC5+RUMBLE+RAM",
+  0x1E "MBC5+RUMBLE+RAM+BATTERY",
+  0x20 "MBC6",
+  0x22 "MBC7+SENSOR+RUMBLE+RAM+BATTERY"
+  0xFC "POCKET CAMERA"
+  0xFD "BANDAI TAMA5"
+  0xFE "HuC3"
+  0xFF "HuC1+RAM+BATTERY"})
 
-(defn file->bytes [path]
-  (let [f (java.io.File. path)
-        ary (byte-array (.length f))
-        is (java.io.FileInputStream. f)]
-    (.read is ary)
-    (.close is)
-    ary))
+(defn file->bytes [f]
+  (.getBytes (slurp f)))
 
 (defn- byte-slice [barr from to]
   (java.util.Arrays/copyOfRange barr from to))
 
 (defn cartridge->header [f]
-  (let [barr (.getBytes (slurp f))]
+  (let [barr (slurp-bytes f)]
     {:entry-point (byte-slice barr 0x100 0x103)
-     :nintendo-logo (byte-slice barr 0x104 0x133)}))
+     :nintendo-logo (byte-slice barr 0x104 0x133)
+     :title (byte-slice barr 0x0134 0x0143)
+     :manufacturer-code (byte-slice barr 0x13F 0x142)
+     :cgb-flag (aget barr 0x143)
+     :new-licensee-code (byte-slice barr 0x144 0x145)
+     :sgb-flag (aget barr 0x146)
+     :cartridge-type (aget barr 0x147)
+     :rom-size (aget barr 0x148)
+     :ram-size (aget barr 0x149)
+     :destination-code (aget barr 0x14A)
+     :old-licensee-code (aget barr 0x14B)
+     :mask-rom-version-number (aget barr 0x14C)
+     :header-checksum (aget barr 0x14D)
+     :global-checksum (byte-slice barr 0x14E 0x14F)}))
 
 (defn load [f]
-  (let [contents (vec (file->bytes f))
-        header (cartridge->header f)]
-    (swap! ctx assoc :rom-size (count contents))
-    {:header header}))
+  (let [header (cartridge->header f)]
+    {:filename f
+     :title (apply str (map char (filter #(not= 0x0 %) (:title header))))
+     :rom-type (get rom-types (:cartridge-type header) "???")
+     :rom-size (:rom-size header)
+     :header header}))
 
+(defn hexify "Convert byte sequence to hex string" [coll]
+  (let [hex [\0 \1 \2 \3 \4 \5 \6 \7 \8 \9 \A \B \C \D \E \F]]
+      (letfn [(hexify-byte [b]
+               (let [v (bit-and b 0xFF)]
+                 [(hex (bit-shift-right v 4)) (hex (bit-and v 0x0F))]))]
+        (apply str (mapcat hexify-byte coll)))))
+
+(defn hexify-str [s]
+  (hexify (.getBytes s)))
+
+(defn slurp-bytes
+  "Slurp the bytes from a slurpable thing"
+  [x]
+  (with-open [in (clojure.java.io/input-stream x)
+              out (java.io.ByteArrayOutputStream.)]
+    (clojure.java.io/copy in out)
+    (.toByteArray out)))
 
 (comment
   (vec (.getBytes "hello"))
   (class (.getBytes "hello"))
 
-  (let [barr (.getBytes "hello world")]
-   (java.util.Arrays/copyOfRange barr 1 2)
-   (java.util.Arrays/copyOfRange barr 3 5))
+  (hexify (.getBytes "DMG-ACID2"))
 
-  (assoc {} :rom-size 100)
+  (let [barr (.getBytes "hello-world")
+        acid-rom-barr (slurp-bytes (io/resource "roms/dmg-acid2.gb"))
+        acid-title (java.util.Arrays/copyOfRange acid-rom-barr 0x134 0x137)
+        acid-byte (aget acid-rom-barr 0x101)]
+    (hexify acid-title))
 
-  (swap! ctx assoc :rom-size 100)
+  (= (.getBytes (slurp (io/resource "roms/dmg-acid2.gb")))
+     (slurp-bytes (io/resource "roms/dmg-acid2.gb")))
 
-  (load "/Users/travis/LLD_gbemu/roms/01-special.gb")
+  (map hexify (partition 64 (.getBytes (slurp (io/resource "roms/01-special.gb")))))
 
-  (file->bytes "/Users/travis/LLD_gbemu/roms/01-special.gb")
-  (string-to-bytes "hello")
+  (load (io/resource "roms/dmg-acid2.gb"))
+
 
   0x100
+  0x10f
+  0x10F
+
+  (Integer/toString -65 16)
 
   nil)
-;; typedef struct {
-;;                 u8 entry[4];
-;;                 u8 logo[0x30];
-
-;;                 char title[16];
-;;                 u16 new_lic_code;
-;;                 u8 sgb_flag;
-;;                 u8 type;
-;;                 u8 rom_size;
-;;                 u8 ram_size;
-;;                 u8 dest_code;
-;;                 u8 lic_code;
-;;                 u8 version;
-;;                 u8 checksum;
-;;                 u16 global_checksum};
-;;  rom_header;
-
-;; #include <cart.h>
-
-;; typedef struct {
-;;     char filename[1024];
-;;     u32 rom_size;
-;;     u8 *rom_data;
-;;     rom_header *header;
-;; } cart_context;
-
-;; static cart_context ctx;
-
-;; static const char *ROM_TYPES[] = {
-;;     "ROM ONLY",
-;;     "MBC1",
-;;     "MBC1+RAM",
-;;     "MBC1+RAM+BATTERY",
-;;     "0x04 ???",
-;;     "MBC2",
-;;     "MBC2+BATTERY",
-;;     "0x07 ???",
-;;     "ROM+RAM 1",
-;;     "ROM+RAM+BATTERY 1",
-;;     "0x0A ???",
-;;     "MMM01",
-;;     "MMM01+RAM",
-;;     "MMM01+RAM+BATTERY",
-;;     "0x0E ???",
-;;     "MBC3+TIMER+BATTERY",
-;;     "MBC3+TIMER+RAM+BATTERY 2",
-;;     "MBC3",
-;;     "MBC3+RAM 2",
-;;     "MBC3+RAM+BATTERY 2",
-;;     "0x14 ???",
-;;     "0x15 ???",
-;;     "0x16 ???",
-;;     "0x17 ???",
-;;     "0x18 ???",
-;;     "MBC5",
-;;     "MBC5+RAM",
-;;     "MBC5+RAM+BATTERY",
-;;     "MBC5+RUMBLE",
-;;     "MBC5+RUMBLE+RAM",
-;;     "MBC5+RUMBLE+RAM+BATTERY",
-;;     "0x1F ???",
-;;     "MBC6",
-;;     "0x21 ???",
-;;     "MBC7+SENSOR+RUMBLE+RAM+BATTERY",
-;; };
-
-;; static const char *LIC_CODE[0xA5] = {
-;;     [0x00] = "None",
-;;     [0x01] = "Nintendo R&D1",
-;;     [0x08] = "Capcom",
-;;     [0x13] = "Electronic Arts",
-;;     [0x18] = "Hudson Soft",
-;;     [0x19] = "b-ai",
-;;     [0x20] = "kss",
-;;     [0x22] = "pow",
-;;     [0x24] = "PCM Complete",
-;;     [0x25] = "san-x",
-;;     [0x28] = "Kemco Japan",
-;;     [0x29] = "seta",
-;;     [0x30] = "Viacom",
-;;     [0x31] = "Nintendo",
-;;     [0x32] = "Bandai",
-;;     [0x33] = "Ocean/Acclaim",
-;;     [0x34] = "Konami",
-;;     [0x35] = "Hector",
-;;     [0x37] = "Taito",
-;;     [0x38] = "Hudson",
-;;     [0x39] = "Banpresto",
-;;     [0x41] = "Ubi Soft",
-;;     [0x42] = "Atlus",
-;;     [0x44] = "Malibu",
-;;     [0x46] = "angel",
-;;     [0x47] = "Bullet-Proof",
-;;     [0x49] = "irem",
-;;     [0x50] = "Absolute",
-;;     [0x51] = "Acclaim",
-;;     [0x52] = "Activision",
-;;     [0x53] = "American sammy",
-;;     [0x54] = "Konami",
-;;     [0x55] = "Hi tech entertainment",
-;;     [0x56] = "LJN",
-;;     [0x57] = "Matchbox",
-;;     [0x58] = "Mattel",
-;;     [0x59] = "Milton Bradley",
-;;     [0x60] = "Titus",
-;;     [0x61] = "Virgin",
-;;     [0x64] = "LucasArts",
-;;     [0x67] = "Ocean",
-;;     [0x69] = "Electronic Arts",
-;;     [0x70] = "Infogrames",
-;;     [0x71] = "Interplay",
-;;     [0x72] = "Broderbund",
-;;     [0x73] = "sculptured",
-;;     [0x75] = "sci",
-;;     [0x78] = "THQ",
-;;     [0x79] = "Accolade",
-;;     [0x80] = "misawa",
-;;     [0x83] = "lozc",
-;;     [0x86] = "Tokuma Shoten Intermedia",
-;;     [0x87] = "Tsukuda Original",
-;;     [0x91] = "Chunsoft",
-;;     [0x92] = "Video system",
-;;     [0x93] = "Ocean/Acclaim",
-;;     [0x95] = "Varie",
-;;     [0x96] = "Yonezawa/s’pal",
-;;     [0x97] = "Kaneko",
-;;     [0x99] = "Pack in soft",
-;;     [0xA4] = "Konami (Yu-Gi-Oh!)"
-;; };
-
-;; const char *cart_lic_name() {
-;;     if (ctx.header->new_lic_code <= 0xA4) {
-;;         return LIC_CODE[ctx.header->lic_code];
-;;     }
-
-;;     return "UNKNOWN";
-;; }
-
-;; const char *cart_type_name() {
-;;     if (ctx.header->type <= 0x22) {
-;;         return ROM_TYPES[ctx.header->type];
-;;     }
-
-;;     return "UNKNOWN";
-;; }
-
-;; bool cart_load(char *cart) {
-;;     snprintf(ctx.filename, sizeof(ctx.filename), "%s", cart);
-
-;;     FILE *fp = fopen(cart, "r");
-
-;;     if (!fp) {
-;;         printf("Failed to open: %s\n", cart);
-;;         return false;
-;;     }
-
-;;     printf("Opened: %s\n", ctx.filename);
-
-;;     fseek(fp, 0, SEEK_END);
-;;     ctx.rom_size = ftell(fp);
-
-;;     rewind(fp);
-
-;;     ctx.rom_data = malloc(ctx.rom_size);
-;;     fread(ctx.rom_data, ctx.rom_size, 1, fp);
-;;     fclose(fp);
-
-;;     ctx.header = (rom_header *)(ctx.rom_data + 0x100);
-;;     ctx.header->title[15] = 0;
-
-;;     printf("Cartridge Loaded:\n");
-;;     printf("\t Title    : %s\n", ctx.header->title);
-;;     printf("\t Type     : %2.2X (%s)\n", ctx.header->type, cart_type_name());
-;;     printf("\t ROM Size : %d KB\n", 32 << ctx.header->rom_size);
-;;     printf("\t RAM Size : %2.2X\n", ctx.header->ram_size);
-;;     printf("\t LIC Code : %2.2X (%s)\n", ctx.header->lic_code, cart_lic_name());
-;;     printf("\t ROM Vers : %2.2X\n", ctx.header->version);
-
-;;     u16 x = 0;
-;;     for (u16 i=0x0134; i<=0x014C; i++) {
-;;         x = x - ctx.rom_data[i] - 1;
-;;     }
-
-;;     printf("\t Checksum : %2.2X (%s)\n", ctx.header->checksum, (x & 0xFF) ? "PASSED" : "FAILED");
-
-;;     return true;
-;; }
